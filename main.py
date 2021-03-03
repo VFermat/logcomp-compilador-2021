fe0645f5a2c3069d5aa11615ee4a26654f82693e
VFermat <vitor.eller30@gmail.com>
2021-03-03 17:56:18 -0300

    first version of compiler

--- main.py
+++ main.py
@@ -1,63 +1,108 @@
 import sys
+from typing import List
+from enum import Enum
 
-numbers = [str(x) for x in range(10)]
-operators = ['+', '-']
+class TokenTypes(Enum):
+    NUMBER = 1
+    EOF = 2
+    PLUS = 3
+    MINUS = 4
 
-def clean_numbers(sentence, numbers, clean):
-    temp = [sentence]
-    for number in clean:
-        temp = number.join(temp).split(number)
-    sentence = temp
-    temp = []
-    for i, number in enumerate(sentence):
-        if i == 0 and number == '':
-            continue
-        for char in number.strip():
-            if char not in numbers:
-                print('Found invalid character')
+class Token:
+
+    tokenType: TokenTypes
+    value: int
+
+    def __init__(self, tokenType: TokenTypes, value: int):
+        self.tokenType = tokenType
+        self.value = value
+
+class Tokenizer:
+
+    position: str
+    position: int
+    actual: Token
+
+    def __init__(self, origin: str):
+        self.origin = origin
+        self.position = 0
+        self.actual = None
+
+    def selectNext(self) -> Token:
+        if self.position == len(self.origin):
+            token = Token(TokenTypes.EOF, 0)
+            self.actual = token
+            return token
+        i = self.position
+        while i < len(self.origin):
+            char = self.origin[i]
+            if char == '+':
+                token = Token(TokenTypes.PLUS, 1)
+                break
+            elif char == '-':
+                token = Token(TokenTypes.MINUS, 1)
+                break
+            elif char.isdigit():
+                temp = char
+                if i < len(self.origin) - 1:
+                    while self.origin[i+1].isdigit():
+                        char = self.origin[i+1]
+                        i += 1
+                        temp += char
+                        if i == len(self.origin) - 1:
+                            break
+                token = Token(TokenTypes.NUMBER, int(temp))
+                break
+            elif char == " ":
+                i += 1
+                continue
+            else:
                 exit(1)
-        temp.append(int(number))
-    return temp
+            i += 1
+        self.actual = token
+        self.position = i+1
+        return token
 
-def clean_operators(sentence, operators, clean):
-    temp = [sentence]
-    for operator in clean:
-        temp = operator.join(temp).split(operator)
-    sentence = temp
-    temp = []
-    for operator in sentence:
-        if operator.strip() in operators:
-            temp.append(operator.strip())
-        elif operator.strip() != '':
-            print('Encountered double operators')
-            exit(1)
-    return temp
+class Parser:
+
+    tokens: Tokenizer
+
+    def __init__(self, code: str):
+        self.tokens = Tokenizer(code)
+
+    def main(self):
+        return self.__parseExpression(self.__run(self.tokens))
+
+    @staticmethod
+    def __parseExpression(tokens: List[Token]) -> int:
+        total = 0
+        i = 0
+        actual = tokens[i]
+        while i < len(tokens) - 1:
+            nextToken = tokens[i+1]
+            if nextToken.tokenType == actual.tokenType or (actual.tokenType in [TokenTypes.PLUS, TokenTypes.MINUS] and nextToken.tokenType in [TokenTypes.PLUS, TokenTypes.MINUS]):
+                raise BufferError()
+            elif actual.tokenType == TokenTypes.PLUS:
+                total += nextToken.value
+            elif actual.tokenType == TokenTypes.MINUS:
+                total -= nextToken.value
+            elif actual.tokenType == TokenTypes.NUMBER and i == 0:
+                total += actual.value
+            actual = nextToken
+            i += 1
+        if tokens[-1].tokenType != TokenTypes.EOF:
+            raise EOFError()
+        return total
+
+    @staticmethod
+    def __run(tokens: Tokenizer):
+        validTokens = []
+        validTokens.append(tokens.selectNext())
+        while tokens.actual.tokenType != TokenTypes.EOF:
+            validTokens.append(tokens.selectNext())
+        return validTokens
+
 
 sentence = sys.argv[1].strip()
-
-if len(sentence) == 0:
-    print("Invalid size")
-    exit(1)
-
-if sentence[-1] in operators:
-    print('Last char is operator')
-    exit(1)
-
-operators_in_sentence = clean_operators(sentence, operators, numbers)
-numbers_in_sentence = clean_numbers(sentence, numbers, operators)
-
-if sentence[0] not in operators:
-    operators_in_sentence = ['+'] + operators_in_sentence
-else:
-    print("Invalid first char")
-    exit(1)
-
-total = 0
-
-for operator, number in zip(operators_in_sentence, numbers_in_sentence):
-    if operator == '+':
-        total += number
-    elif operator == '-':
-        total -= number
-
-print(total)
+parser = Parser(sentence)
+print(parser.main())
