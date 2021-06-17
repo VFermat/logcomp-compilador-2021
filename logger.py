d56b0f898f9ff5c843beb8b7eade6c7bcb317861
VFermat <vitor.eller30@gmail.com>
2021-06-17 16:18:30 -0300

    Correct logger erro that avoided functional working

--- logger.py
+++ logger.py
@@ -1,33 +1,36 @@
+import os
 
-class Logger():
 
+class Logger:
     def __init__(self, logfile):
         super().__init__()
+        if not os.path.isdir("out"):
+            os.mkdir("out")
         self.logfile = logfile
         self._cleanFiles()
 
     def _cleanFiles(self):
-        with open(self.logfile, 'w') as f:
-            f.write('')
-        with open('./out/ast.log', 'w') as f:
-            f.write('')
+        with open(self.logfile, "w") as f:
+            f.write("")
+        with open("./out/ast.log", "w") as f:
+            f.write("")
 
     def logParse(self, msg):
-        with open(self.logfile, 'a') as f:
+        with open(self.logfile, "a") as f:
             f.write(msg)
-            f.write('\n')
+            f.write("\n")
 
     def logAst(self, ast, depth: int = 0):
-        with open('./out/ast.log', 'a') as f:
-            tabs = " "*depth
-            f.write(f'{tabs}[Node {ast} ')
+        with open("./out/ast.log", "a") as f:
+            tabs = " " * depth
+            f.write(f"{tabs}[Node {ast} ")
             if type(ast) is not list:
-                f.write(f'{ast.value.value}')
-            f.write(' ]:')
-            f.write('\n')
+                f.write(f"{ast.value.value}")
+            f.write(" ]:")
+            f.write("\n")
             if type(ast) is list:
                 for child in ast:
-                    self.logAst(child, depth+1)
+                    self.logAst(child, depth + 1)
             else:
                 for child in ast.children:
-                    self.logAst(child, depth+1)
+                    self.logAst(child, depth + 1)
